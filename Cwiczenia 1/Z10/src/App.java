a8bf91c110eec7e5e80570c2e44a44fb4ae587ba
Daniel Kucharski <danku@onet.eu>
2021-10-11 13:29:38 +0200

    Create

--- Cwiczenia 1/Z10/src/App.java
+++ Cwiczenia 1/Z10/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("1.Skazani na Shawshank\n2.Nietykalni\n3.Zielona mila\n\n");
     }
 }
